b2a82d3ee296275f7af50055cc5e7fef5f5bbf76
Rick Belson <belsonrj@aol.com>
2020-07-29 19:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function fetchBooks() {
     .then(json => renderBooks(json))
 }
 
-function renderBooks(json) {
+function getPosts(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
